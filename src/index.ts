5fd38c13f81cdc0ac0f2325e9ecf902b38addae9
이요환 <91039622+bumstead-bumstead@users.noreply.github.com>
2022-10-14 23:03:09 +0900

    create /new API

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,13 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import { DataSource } from 'typeorm';
 import dotenv from 'dotenv';
+import { Game } from './entities/game';
 
 dotenv.config();
 
 export const AppDataSource = new DataSource({
-  type: 'postgres',
-  host: process.env.DATABASE_HOST,
-  port: 5432,
-  username: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
+  type: 'sqlite',
+  database: 'database.sqlite',
   synchronize: true,
   logging: false,
   entities: ['src/entities/*.ts'],
@@ -34,13 +31,28 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.post('/new',async (req:Request, res: Response) => {
+  const result : object[] = [];
+  const game = await AppDataSource.getRepository(Game).create({ 
+    name: req.body.name,
+    background: req.body.background,
+    showcase: req.body.showcase,
+    result: JSON.stringify(result)
+   });
+  const gameResult = await AppDataSource.getRepository(Game).save(game);
+   
+  return res.send(gameResult);
+})
+
 app.get('/', async (req: Request, res: Response) => {
   res.send(200);
 });
 
-app.post('/upload', upload.single('image'), async (req, res) => {
-  res.send({ url: (req.file as any).location });
-});
+// app.post('/upload', upload.single('image'), async (req, res) => {
+//   res.send({ url: (req.file as any).location });
+// });
+
+app.post('/new', )
 
 const PORT = 4000;
 
@@ -54,11 +66,10 @@ try {
 
 function drinkCode(mainIdxs : number[], subIdxs : number[], ganish : number) {
   //똑부러짐 뚝딱거림 귀여움 시크함 다정함 무뚝뚝함 투머치토커 과묵함 유잼 노잼 섬세함 둔감함 대식가 소식가 호불호확실 팔랑귀 밖순이 집순이 연애고수 연애고자
-  let answer = 0;
   let cnt1 = 0;
   let cnt2 = 0;
 
-  let arr = [[30,30,20,20], [30,30,20,20], [130,130,120,120], [130,130,120,120]];
+  const arr = [[30,30,20,20], [30,30,20,20], [130,130,120,120], [130,130,120,120]];
   for (let i=0; i < mainIdxs.length; i++) {
      cnt1 += (mainIdxs[i] + 1) % 2;
      cnt2 += (subIdxs[i] + 1) % 2;
